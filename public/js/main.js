ecd3894b31d2a8e8dab4b342c6181b680193ccae
Amir Malik <amir@newb.hackerdojo.com>
2010-11-20 12:43:35 -0800

    initial drop

---
+++ public/js/main.js
@@ -0,0 +1,19 @@
+// main application
+
+HashHistory.initialize();
+HashHistory.register(/^#test/, function(hash) {
+  console.log('test hash: ' + hash);
+});
+HashHistory.loadURL(function() {
+  // we're called in case no fragment is matched
+  console.log('no fragments matched.');
+});
+//HashHistory.save("#fragment");
+
+function Shopicient() {
+}
+
+Shopicient.prototype.run = function() {
+
+};
+
